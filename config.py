15c65cef3b11fda48dfd82586211ed14d37b658d
TAG-Epic <admin@itzepic.net>
2020-10-13 19:44:07 +0200

    Fix: redis default db should be 0 not 1

--- config.py
+++ config.py
@@ -13,6 +13,6 @@ MONGO_DATABASE = env["mongo_database"]
 
 # Redis
 REDIS_HOST = env["redis_host"]
-REDIS_DB = int(env.get("redis_db", 1))
+REDIS_DB = int(env.get("redis_db", 0))
 REDIS_PASSWORD = env.get("redis_password", None)
 REDIS_USERNAME = env.get("redis_username", None)
