e71b8f9592c7442d1e457eb40cc36431a0eb5843
TAG-Epic <admin@itzepic.net>
2020-10-14 19:01:06 +0200

    Feat: automatically delete lobbies when a game is done

--- cogs/queue.py
+++ cogs/queue.py
@@ -9,15 +9,18 @@ from discord.ext.commands import Cog
 from discord import VoiceState, Member
 from asyncio import Lock
 from typing import Dict
+from logging import getLogger
 
 
 class Queue(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
+        self.logger = getLogger("AQue.cogs.queue")
         self.locks: Dict[int, Dict[str, Lock]] = {}
         self.lobby_channels: Dict[int, Dict[str, int]] = {}
         self.game_channels = []
         self.lobby_users = 1
+        self.lobby_deletion_threshold = 0
 
     def is_lobby_vc(self, channel: int, guild_id: int):
         return any([channel_id == channel for channel_id in list(self.lobby_channels[guild_id].values())])
@@ -53,7 +56,7 @@ class Queue(Cog):
                 guild_lobbies[match_type] = lobby_vc
                 self.lobby_channels[guild.id] = guild_lobbies
             voice = self.bot.get_channel(lobby_vc)
-            await member.move_to(voice, reason="Assembling lobby")
+            await member.move_to(voice, reason="[AQue] Assembling lobby")
 
     @Cog.listener("on_voice_state_update")
     async def start_games(self, member: Member, before: VoiceState, after: VoiceState):
@@ -76,11 +79,28 @@ class Queue(Cog):
         async with self.locks[guild.id][game_type]:
             if len(voice_channel.members) < self.lobby_users:
                 return
-            await voice_channel.edit(category=in_game_category, name="Use /code <code>")
+            await voice_channel.edit(category=in_game_category, name="Use /code <code>", reason="[AQue] Lobby found.")
             del self.lobby_channels[guild.id][game_type]
 
-    async def post_queue_stats(self, queue_game: str, guild_name: str,  queue_size: int):
+    @Cog.listener("on_voice_state_update")
+    async def delete_lobbies_on_empty(self, member: Member, before: VoiceState, after: VoiceState):
+        if after.channel is not None and before.channel != after.channel and not member.bot:
+            return
+        guild = member.guild
+        api: Api = self.bot.get_cog("Api")
+        guild_config = api.get_server_settings(guild)
+
+        if guild_config is None:
+            return
+        in_game_category_id = guild_config["categories"]["in_game"]
+        if before.channel.category_id != in_game_category_id:
+            return
+        if len(before.channel.members) <= self.lobby_deletion_threshold:
+            await before.channel.delete(reason="[AQue] Lobby is empty.")
+
+    async def post_queue_stats(self, queue_game: str, guild_name: str, queue_size: int):
         pass  # Kekw cant be assed
+
 
 def setup(bot: Bot):
     bot.add_cog(Queue(bot))
