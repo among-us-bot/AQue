55a7f515b5ddd10d87af882ab27c207d0070d306
TAG-Epic <admin@itzepic.net>
2020-10-14 23:07:30 +0200

    Feat: Add new setup channels and remove in game category

--- cogs/setup.py
+++ cogs/setup.py
@@ -13,6 +13,13 @@ class Setup(Cog):
         self.bot = bot
         self.setup_lock = Lock()
 
+        self.not_verified_message = """
+        **You have not linked your Among Us username**
+        In order to access matchmaking you have to link your account.
+        Simply send /link <username>
+        So something like /link Hooman
+        """.replace("    ", "")
+
     @command()
     @has_permissions(manage_guild=True)
     async def setup(self, ctx: Context):
@@ -33,11 +40,32 @@ class Setup(Cog):
 
                 matchmaking_category_permissions = {
                     configured_user_role: PermissionOverwrite(read_messages=True),
+                    ctx.guild.default_role: PermissionOverwrite(read_messages=False, speak=False)
+                }
+
+                banned_channel_permissions = {
+                    banned_user_role: PermissionOverwrite(read_messages=True),
+                    ctx.guild.default_role: PermissionOverwrite(read_messages=False, connect=False)
+                }
+
+                non_linked_channel_permissions = {
+                    configured_user_role: PermissionOverwrite(read_messages=False),
+                    banned_user_role: PermissionOverwrite(read_messages=False),
+                    ctx.guild.default_role: PermissionOverwrite(connect=False, send_messages=False)
+                }
+
+                hidden_permissions = {
                     ctx.guild.default_role: PermissionOverwrite(read_messages=False)
                 }
 
                 matchmaking_category = await ctx.guild.create_category(name="Matchmaking! Join to queue!",
                                                                        overwrites=matchmaking_category_permissions)
+                non_linked_channel = await matchmaking_category.create_text_channel(name="Please link your account",
+                                                                                    overwrites=non_linked_channel_permissions)
+                await non_linked_channel.send(self.not_verified_message)
+
+                await matchmaking_category.create_voice_channel(name="You have been banned!",
+                                                                overwrites=banned_channel_permissions)
                 matchmaking_na_normal = await matchmaking_category.create_voice_channel(name="NA-Normal")
                 matchmaking_eu_normal = await matchmaking_category.create_voice_channel(name="EU-Normal")
                 matchmaking_asia_normal = await matchmaking_category.create_voice_channel(name="ASIA-Normal")
@@ -48,13 +76,12 @@ class Setup(Cog):
                     "asia-normal": matchmaking_asia_normal.id
                 }
 
-                lobby_category = await ctx.guild.create_category("Lobby! Waiting for players")
-                in_game_category = await ctx.guild.create_category("In game! Leave when you are done")
+                lobby_category = await ctx.guild.create_category("Lobby! Waiting for players",
+                                                                 overwrites=hidden_permissions)
 
                 categories = {
                     "matchmaking": matchmaking_category.id,
                     "lobby": lobby_category.id,
-                    "in_game": in_game_category.id
                 }
 
                 config = {
